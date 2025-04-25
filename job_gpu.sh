d248576eb044e96bc3475702845d279b8f21ba13
ftmp <ftmp@dtu.dk>
2025-04-25 12:29:46 +0200

    ran forecast on remote

---
+++ job_gpu.sh
@@ -0,0 +1,32 @@
+#!/bin/sh 
+### General options 
+### -- specify queue -- 
+#BSUB -q gpua100
+### -- set the job Name -- 
+#BSUB -J forecasts
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 4
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=5GB]"
+### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
+#BSUB -M 5GB
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 24:00 
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o outputs_hpc/Output_%J.out 
+#BSUB -e outputs_hpc/Output_%J.err 
+
+
+nvidia-smi
+# Load the cuda module
+module load cuda/11.6
+
+source ../envs/envs/shred/bin/activate
+
+# here follow the commands you want to execute with input.in as the input file
+python -u forecasts.py
